33d200184756166556990befff3ec15652b33d7b
cdeleon-2021671 <cdeleon-2021671@kinal.edu.gt>
2023-11-03 09:27:37 -0600

    Aceptar dominios

--- config/app.js
+++ config/app.js
@@ -33,8 +33,6 @@ const corsOptions = {
       callback(new Error("Dominio no permitido por CORS"));
     }
   },
-  exposeHeaders: ['Referer'],
-  credentials: true
 };
 
 // Utilizar cors en el servidor con la configuracion para tienda.gt
